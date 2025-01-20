0c5c12c7d6527aeb03a95ef54b45fe381c7b0ac3
Rostislav Statko <rostislav.statko@gmail.com>
2025-01-20 13:05:44 +0300

    fix: robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next'
 
 import { getAppUrl } from '@/utils/urls'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {

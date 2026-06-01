ba40f441313aaecef3036a8df2bd4bf1a4874962
Rostislav Statko <contact@dendeline.com>
2026-06-01 19:37:55 +0300

    chore: enable dev access from mobile

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from 'next'
 
+const devOrigins = process.env.ALLOWED_DEV_ORIGINS
+  ? process.env.ALLOWED_DEV_ORIGINS.split(',')
+  : []
+
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
   typedRoutes: true,
+  allowedDevOrigins: devOrigins,
 }
 
 export default nextConfig
